1946059625e4e5f3a546061fe746addbbd7c6878
Andre Rochlitz <Andre.Rochlitz@kv.digital>
2024-06-03 13:38:38 +0200

    K2Converter add test

---
+++ src/test/java/org/rochlitz/K2Converter/sqlConverter/InsertToSqlConverterTest.java
@@ -0,0 +1,61 @@
+package org.rochlitz.K2Converter.sqlConverter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.rochlitz.K2Converter.sqlConverter.SqlTemplates.INSERT_INTO;
+import static org.rochlitz.K2Converter.sqlConverter.SqlTemplates.INSERT_VALUES;
+import static org.rochlitz.K2Converter.sqlConverter.SqlTemplates.SEMICOLON;
+
+import java.util.Arrays;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.support.DefaultExchange;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.rochlitz.K2Converter.Context;
+import org.rochlitz.K2Converter.toTypeConverter.FeldRecord;
+import org.rochlitz.K2Converter.toTypeConverter.InsertRecord;
+
+public class InsertToSqlConverterTest {
+
+    private InsertToSqlConverter converter;
+    private Exchange exchange;
+    private CamelContext camelContext;
+
+    @BeforeEach
+    public void setUp() {
+        converter = new InsertToSqlConverter();
+        exchange = new DefaultExchange(new DefaultCamelContext());
+    }
+
+    @Test
+    public void testProcess_HappyPath() throws Exception {
+        // Prepare mock objects
+        InsertRecord insertRecord = new InsertRecord();
+        insertRecord.setValues(Arrays.asList("value1"));
+        Context.setTableName("my_table");
+
+        FeldRecord record = new FeldRecord();
+        record.setPrimaryKey(false);
+        record.setNullable(false);
+
+
+        record.setFieldName("field1");
+        Context.setTableInfo(record);
+
+        exchange.getIn().setBody(insertRecord);
+
+        // Call the process method
+        new InsertToSqlConverter().process(exchange);
+
+        // Verify the generated SQL statement
+        String expectedSql = String.format(INSERT_INTO, "my_table", "field1") +
+            String.format(INSERT_VALUES, "'value1'") + SEMICOLON;
+        assertEquals(expectedSql, exchange.getIn().getBody(String.class));
+    }
+
+
+
+
+}
